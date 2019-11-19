9a3ff40b6777961265a546484b5653705873c3b2
samuvait <sav.v@hotmail.fi>
2019-11-19 13:23:22 +0200

    Added high res image to preview

--- vyme/src/components/GridGallery.js
+++ vyme/src/components/GridGallery.js
@@ -3,19 +3,23 @@ import Gallery from 'react-grid-gallery'
 
 const GridGallery = ({ movies }) => {
 
-  const posterUrl = 'https://image.tmdb.org/t/p/w185'
+  const posterUrl = 'https://image.tmdb.org/t/p'
+  //options for image size, pixel width. 'original' also possible
+  const small = '/w185'
+  const large = '/w500'
   const shortDescLength = 40
 
   const IMAGES = movies.map(movie => {
     const movieName = movie.title
     const longDesc = movie.overview
     const description = `${longDesc.substring(0, shortDescLength)}...`
-    const posterPath = `${posterUrl}${movie.poster_path}`
+    const posterPath = `${posterUrl}${small}${movie.poster_path}`
+    const fullPosterPath = `${posterUrl}${large}${movie.poster_path}`
     const avrg = movie.vote_average
     const genres = movie.genre_ids
 
     return ({
-      src: posterPath,
+      src: fullPosterPath,
       thumbnail: posterPath,
       thumbnailWidth: 185,
       thumbnailHeight: 278,
